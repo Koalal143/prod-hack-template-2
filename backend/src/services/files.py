bd0fe945d89741e2b68d15242d086dd113df83c3
Sansly25 <robertstepa017@gmail.com>
2025-11-09 17:26:15 +0300

    feat(files): add upload and download methods in FileService

--- backend/src/services/files.py
+++ backend/src/services/files.py
@@ -1,12 +1,16 @@
-from aiobotocore.client import AioBaseClient
+import uuid
 
+from aiobotocore.client import AioBaseClient
+from fastapi import Depends
+
+from core.s3.dependencies import get_s3_client
 from src.settings import settings
 
 class FileService:
     def __init__(self, s3_client: AioBaseClient):
         self.s3_client = s3_client
 
-    async def _get_presigned_url(self, operation_type: str, key: str):
+    async def _get_presigned_url(self, operation_type: str, key: str) -> str:
         return await self.s3_client.generate_presigned_url(
             operation_type,
             Params={
@@ -16,8 +20,13 @@ class FileService:
             ExpiresIn=3600,
         )
 
-    async def get_upload_url(self, key: str):
+    async def get_upload_url(self, filename):
+        key = f"{str(uuid.uuid4())}_{filename}"
         return await self._get_presigned_url("put_object", key)
 
     async def get_download_url(self, key: str):
         return await self._get_presigned_url("get_object", key)
+
+
+async def get_files_service(s3_client: AioBaseClient = Depends(get_s3_client)):
+    return FileService(s3_client)
