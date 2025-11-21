c220c9435b0bbe572fab854a9ef10d7d1ccff7ee
QuickFox43 <koalal1428@gmail.com>
2025-11-21 13:22:21 +0300

    fix(init): add importing health

--- backend/src/api/v1/__init__.py
+++ backend/src/api/v1/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from src.api.v1 import files, users
+from src.api.v1 import files, users, health
 
 v1_router = APIRouter(prefix="/v1")
 
 
-for module in (users, files):
+for module in (users, files, health):
     v1_router.include_router(module.router)
